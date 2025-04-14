b698dfb60bd94449727ec7737dafc1135d41b661
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-04-14 14:27:52 -0400

    Set flag to include kernel names to true.

--- src/roofline.py
+++ src/roofline.py
@@ -56,7 +56,7 @@ class Roofline:
                 "device_id": 0,
                 "sort_type": "kernels",
                 "mem_level": "ALL",
-                "include_kernel_names": False,
+                "include_kernel_names": True,
                 "is_standalone": False,
             }
         )
